02e283fa963437fb403c13dbabbab7b42425b332
HuiwonHwang <ct09md@gmail.com>
2026-03-30 12:54:22 +0900

    2026-03-30

---
+++ java_jsp_project/java_pjt/src/a_basic/Java_01.java
@@ -0,0 +1,17 @@
+	package a_basic;
+
+public class Java_01 {
+
+	public static void main(String[] args) {
+		/*
+		정수: byte,1byte             short,2byte        int,4byte             long,8byte
+		     -(2^7)~(2^7)-1         ~(2^15)-1               ~2147483647                      992경 10L;
+		실수: float, double
+		*/
+		
+		byte num1 = 127;
+		
+		
+	}
+
+}
